37a3ff3443c9cb53ec73e7dcabb74f27eb043652
Jeong-wonho <kallzzang@naver.com>
2023-04-18 21:22:23 +0900

    main.py 작성

---
+++ app/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.common.config import conf
+
+def creae_app():
+    """
+    앱 함수 실행
+    """
+
+    # c = conf()
+    app = FastAPI()
+
+    # 데이터 베이스 이니셜라이즈
+    # 레디스 이니셜라이즈
+    # 미들웨어 정의
+    # 라우터 정의
+
+    return app
+
+app = creae_app();
+
+print(__name__);
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
