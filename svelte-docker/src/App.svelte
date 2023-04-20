82d4d849a02cf8172884f1ad809008f86d0542c6
Jeong-wonho <kallzzang@naver.com>
2023-04-20 11:08:35 +0900

    frontend backend 연결

--- svelte-docker/src/App.svelte
+++ svelte-docker/src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
-</script>
+	async function hello() {
+		const res = await fetch("http://127.0.0.1:8000/hello");
+		const json = await res.json();
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+		if (res.ok) {
+			return json.message;
+		} else {
+			alert("error");
 		}
 	}
-</style>
+
+	let promise = hello();
+</script>
+
+{#await promise}
+	<p>...waiting</p>
+{:then message}
+	<h1>{message}</h1>
+{/await}
