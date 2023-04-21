9372b30b8da201025aa5193b307c7e5773b1b9e6
Jeong-wonho <kallzzang@naver.com>
2023-04-21 16:07:56 +0900

    error 추가 , bootstrap 추가

--- thisfastapi/frontend/src/main.js
+++ thisfastapi/frontend/src/main.js
@@ -1,4 +1,8 @@
+// @ts-ignore
 import App from "./App.svelte"
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
 
 const app = new App({
   target: document.getElementById('app'),
