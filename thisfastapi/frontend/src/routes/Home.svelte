578d2a20d16353c410d56d341c8fe6705a949c1b
Jeong-wonho <kallzzang@naver.com>
2023-04-24 15:24:32 +0900

    페이징 처러 변경 추가!

--- thisfastapi/frontend/src/routes/Home.svelte
+++ thisfastapi/frontend/src/routes/Home.svelte
@@ -1,67 +1,90 @@
 <script>
-    import fastapi from "../lib/api"
-    import {link} from 'svelte-spa-router'
+  import fastapi from "../lib/api";
+  import { link } from "svelte-spa-router";
 
-    let question_list = []
-    let size = 10
-    let page = 0
-    let total = 0 
-    $: total_page = Math.ceil(total/size)
+  let question_list = [];
+  let size = 10;
+  let page = 0;
+  let total = 0;
+  const maxButton = 10;
+  let currentPage = 1;
+  let page_list = []
 
-    function get_question_list(_page) {
-        let params = {
-            page : _page,
-            size : size,
-        }
-        fastapi('get', '/api/question/list', params, (json) => {
-            question_list = json.question_list
-            page = _page
-            total = json.total
-        })
+  $: total_page = Math.ceil(total / size);
+
+  function get_question_list(_page) {
+    let params = {
+      page: _page,
+      size: size,
+    };
+    fastapi("get", "/api/question/list", params, (json) => {
+      question_list = json.question_list;
+      page = _page;
+      total = json.total;
+    });
+  }
+
+  get_question_list(0);
+  
+  function customRange(start, stop, total_page) {
+    if (stop > total_page) {
+      stop = total_page
     }
-
-    get_question_list(0)
+    return Array.from({length: stop - start}, (_, i) => i + start);
+  }
+  
 </script>
 
 <div class="container my-3">
-    <table class="table">
-        <thead>
-        <tr class="table-dark">
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성일시</th>
-        </tr>
-        </thead>
-        <tbody>
-        {#each question_list as question, i}
+  <table class="table">
+    <thead>
+      <tr class="table-dark">
+        <th>번호</th>
+        <th>제목</th>
+        <th>작성일시</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each question_list as question, i}
         <tr>
-            <td>{i+1}</td>
-            <td>
-                <a use:link href="/detail/{question.id}">{question.subject}</a>
-            </td>
-            <td>{question.create_date}</td>
+          <td>{i + 1}</td>
+          <td>
+            <a use:link href="/detail/{question.id}">{question.subject}</a>
+          </td>
+          <td>{question.create_date}</td>
         </tr>
-        {/each}
-        </tbody>
-    </table>
-    <!-- 페이징 처리 시작 -->
-    <ul class="pagination justify-content-center">
-        <!-- 이전페이지 -->
-        <li class="page-item {page <= 0 && 'disabled'}">
-            <button class="page-link" on:click="{() => get_question_list(page-1)}">이전</button>
-        </li>
-        <!--페이지번호-->
-        {#each Array(total_page) as _, loop_page}
-        {#if loop_page >= page-5 && loop_page <= page+5}
+      {/each}
+    </tbody>
+  </table>
+  
+
+  <!-- 페이징 처리 시작 -->
+  <ul class="pagination justify-content-center">
+    <!-- 이전페이지 -->
+    <li class="page-item {page <= 0 && 'disabled'}">
+      <button class="page-link" on:click={() => get_question_list(page - 10)}
+        >이전</button
+      >
+    </li>
+    <!--페이지번호-->
+
+    {#each customRange(page, page+10, total_page) as loop_page}
+      <!-- {#if loop_page >= page-5 && loop_page <= total_page}  -->
         <li class="page-item {loop_page === page && 'active'}">
-            <button on:click="{()=> get_question_list(loop_page)}" class="page-link">{loop_page+1}</button>
+          <button
+            on:click={() => get_question_list(loop_page)}
+            class="page-link">{loop_page + 1}</button
+          >
         </li>
-        {/if}
-        {/each}
-        <!--다음페이지-->
-        <li class="page-item {page>= total_page-1 && 'disabled'}">
-            <button class="page-link" on:click="{() => get_question_list(page+1)}">다음</button>
-        </li>
-    </ul>
-    <a use:link href="/question-create" class="btn btn-primary">질문 등록 하기</a>
+      <!-- {/if} -->
+    {/each}
+    <!--다음페이지-->
+    <li class="page-item {page >= total_page - 1 && 'disabled'}">
+      <button class="page-link" on:click={() => get_question_list(page + 10)}
+        >다음</button
+      >
+    </li>
+  </ul>
+
+  <a use:link href="/question-create" class="btn btn-primary">질문 등록 하기</a>
 </div>
